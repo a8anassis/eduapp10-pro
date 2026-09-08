eef6e2ccbe6f4d4ab08d78b8feb64fa2d8e6b054
a8anassis <a8anassis@gmail.com>
2026-09-08 20:08:00 +0300

    feat(application): enable resilient methods for improved fault tolerance

--- src/main/java/gr/aueb/cf/eduapp/EduAppApplication.java
+++ src/main/java/gr/aueb/cf/eduapp/EduAppApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.resilience.annotation.EnableResilientMethods;
 
 @SpringBootApplication
+@EnableResilientMethods
 @EnableJpaAuditing
 public class EduAppApplication {
 
